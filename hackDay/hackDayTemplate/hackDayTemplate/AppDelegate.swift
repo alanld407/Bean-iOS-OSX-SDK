6d0e5ef9708d40c1fd6b1ac8297e12d32cb16ad7
Rene Limberger <rene.limberger@disneyanimation.com>
2014-09-23 13:23:49 -0700

    add hack day template example app

---
+++ hackDay/hackDayTemplate/hackDayTemplate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  hackDayTemplate
+//
+//  Created by Rene Limberger on 9/23/14.
+//  Copyright (c) 2014 Walt Disney Animation. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ hackDay/hackDayTemplate/hackDayTemplate/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  hackDayTemplate
+//
+//  Created by Rene Limberger on 9/23/14.
+//  Copyright (c) 2014 Walt Disney Animation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, PTDBeanManagerDelegate {
+    
+    let beanManager = PTDBeanManager()
+    var currentBean: PTDBean?
+    
+    @IBOutlet weak var rightEar: UIButton!
+    @IBOutlet weak var leftEar: UIButton!
+    @IBOutlet weak var speak: UIButton!
+    
+    let colors = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor()]
+    var currentColorIndex = 0
+    
+    var currentSoundIndex = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        beanManager.delegate = self
+        
+        //hideButtons()
+    }
+    
+    func hideButtons() {
+        rightEar.hidden = true
+        leftEar.hidden = true
+        speak.hidden = true
+    }
+    
+    func showButtons() {
+        rightEar.hidden = false
+        leftEar.hidden = false
+        speak.hidden = false
+    }
+
+    @IBAction func cycleEarColor(sender: UIButton) {
+        currentColorIndex++
+        if currentColorIndex >= colors.count {
+            currentColorIndex = 0
+        }
+        setColor(colors[currentColorIndex])
+    }
+    
+    func setColor(color: UIColor) {
+        // color buttons
+        rightEar.backgroundColor = color
+        leftEar.backgroundColor = color
+        
+        if let bean = currentBean {
+            bean.setLedColor(color)
+        }
+    }
+    
+    @IBAction func saySomething(sender: UIButton) {
+        currentSoundIndex++
+        if currentSoundIndex >= 3 {
+            currentSoundIndex = 0
+        }
+        setSound(currentColorIndex)
+    }
+    
+    func setSound(soundIndex: Int) {
+        // send sound code
+        if let bean = currentBean {
+            bean.sendSerialString("sound: \(soundIndex)")
+        }
+    }
+    
+    func beanManagerDidUpdateState(beanManager: PTDBeanManager!) {
+        if beanManager.state == BeanManagerState.PoweredOn {
+            beanManager.startScanningForBeans_error(nil)
+        }
+    }
+    
+    func BeanManager(beanManager: PTDBeanManager!, didDiscoverBean bean: PTDBean!, error: NSError!) {
+        if bean.name == "WADS Bean" {
+            beanManager.connectToBean(bean, error: nil)
+        }
+    }
+    
+    func BeanManager(beanManager: PTDBeanManager!, didConnectToBean bean: PTDBean!, error: NSError!) {
+        showButtons()
+        currentBean = bean
+    }
+    
+    func BeanManager(beanManager: PTDBeanManager!, didDisconnectBean bean: PTDBean!, error: NSError!) {
+        hideButtons()
+        currentBean = nil
+    }
+
+}
+
